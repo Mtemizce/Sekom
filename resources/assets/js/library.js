4e6ec2b8d9128c3d1c04a0d204eb1ffb83d13584
mtemizce <mehmettemizce@hotmail.com.tr>
2018-12-19 00:45:42 +0300

    .

---
+++ resources/assets/js/library.js
@@ -0,0 +1,87 @@
+var LIBS = {
+	// Chart libraries
+	plot: [
+		"../sekom/resources/libs/misc/flot/jquery.flot.min.js",
+		"../sekom/resources/libs/misc/flot/jquery.flot.pie.min.js",
+		"../sekom/resources/libs/misc/flot/jquery.flot.stack.min.js",
+		"../sekom/resources/libs/misc/flot/jquery.flot.resize.min.js",
+		"../sekom/resources/libs/misc/flot/jquery.flot.curvedLines.js",
+		"../sekom/resources/libs/misc/flot/jquery.flot.tooltip.min.js",
+		"../sekom/resources/libs/misc/flot/jquery.flot.categories.min.js"
+	],
+	chart: [
+		'../sekom/resources/libs/misc/echarts/build/dist/echarts-all.js',
+		'../sekom/resources/libs/misc/echarts/build/dist/theme.js',
+		'../sekom/resources/libs/misc/echarts/build/dist/jquery.echarts.js'
+	],
+	circleProgress: [
+		"../sekom/resources/libs/bower/jquery-circle-progress/dist/circle-progress.js"
+	],
+	sparkline: [
+		"../sekom/resources/libs/misc/jquery.sparkline.min.js"
+	],
+	maxlength: [
+		"../sekom/resources/libs/bower/bootstrap-maxlength/src/bootstrap-maxlength.js"
+	],
+	tagsinput: [
+		"../sekom/resources/libs/bower/bootstrap-tagsinput/dist/bootstrap-tagsinput.css",
+		"../sekom/resources/libs/bower/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js",
+	],
+	TouchSpin: [
+		"../sekom/resources/libs/bower/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.css",
+		"../sekom/resources/libs/bower/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.js"
+	],
+	selectpicker: [
+		"../sekom/resources/libs/bower/bootstrap-select/dist/css/bootstrap-select.min.css",
+		"../sekom/resources/libs/bower/bootstrap-select/dist/js/bootstrap-select.min.js"
+	],
+	filestyle: [
+		"../sekom/resources/libs/bower/bootstrap-filestyle/src/bootstrap-filestyle.min.js"
+	],
+	timepicker: [
+		"../sekom/resources/libs/bower/bootstrap-timepicker/js/bootstrap-timepicker.js"
+	],
+	datetimepicker: [
+		"../sekom/resources/libs/bower/moment/moment.js",
+		"../sekom/resources/libs/bower/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css",
+		"../sekom/resources/libs/bower/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"
+	],
+	select2: [
+		"../sekom/resources/libs/bower/select2/dist/css/select2.min.css",
+		"../sekom/resources/libs/bower/select2/dist/js/select2.full.min.js"
+	],
+	vectorMap: [
+		"../sekom/resources/libs/misc/jvectormap/jquery-jvectormap.css",
+		"../sekom/resources/libs/misc/jvectormap/jquery-jvectormap.min.js",
+		"../sekom/resources/libs/misc/jvectormap/maps/jquery-jvectormap-us-mill.js",
+		"../sekom/resources/libs/misc/jvectormap/maps/jquery-jvectormap-world-mill.js",
+		"../sekom/resources/libs/misc/jvectormap/maps/jquery-jvectormap-africa-mill.js"
+	],
+	summernote: [
+		"../sekom/resources/libs/bower/summernote/dist/summernote.css",
+		"../sekom/resources/libs/bower/summernote/dist/summernote.min.js"
+	],
+	DataTable: [
+		"../sekom/resources/libs/misc/datatables/datatables.min.css",
+		"../sekom/resources/libs/misc/datatables/datatables.min.js"
+	],
+	fullCalendar: [
+		"../sekom/resources/libs/bower/moment/moment.js",
+		"../sekom/resources/libs/bower/fullcalendar/dist/fullcalendar.min.css",
+		"../sekom/resources/libs/bower/fullcalendar/dist/fullcalendar.min.js"
+	],
+	dropzone: [
+		"../sekom/resources/libs/bower/dropzone/dist/min/dropzone.min.css",
+		"../sekom/resources/libs/bower/dropzone/dist/min/dropzone.min.js"
+	],
+	counterUp: [
+		"../sekom/resources/libs/bower/waypoints/lib/jquery.waypoints.min.js",
+		"../sekom/resources/libs/bower/counterup/jquery.counterup.min.js"
+	],
+	others: [
+		"../sekom/resources/libs/bower/switchery/dist/switchery.min.css",
+		"../sekom/resources/libs/bower/switchery/dist/switchery.min.js",
+		"../sekom/resources/libs/bower/lightbox2/dist/css/lightbox.min.css",
+		"../sekom/resources/libs/bower/lightbox2/dist/js/lightbox.min.js"
+	]
+};
